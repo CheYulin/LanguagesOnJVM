68afab4271413c92ef15e95086e987f6fbad3987
cheyulin <yche@cse.ust.hk>
2016-12-06 16:04:31 +0800

    add study of sbt project

---
+++ Scala/SBTProject/build.sbt
@@ -0,0 +1,6 @@
+name := "SBTProject"
+
+version := "1.0"
+
+scalaVersion := "2.12.1"
+    
---
+++ Scala/SBTProject/src/main/scala/org/yche/demo/HelloWorld.scala
@@ -0,0 +1,10 @@
+package org.yche.demo
+
+/**
+  * Created by cheyulin on 12/6/16.
+  */
+object HelloWorld {
+  def main(args: Array[String]): Unit = {
+    println("Hello World!")
+  }
+}
